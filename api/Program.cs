f03164684ba4a739c608fec5fbf7d26181dca9dc
caowens <caowens555@yahoo.com>
2025-11-20 20:19:10 -0800

    Connect to DB

--- api/Program.cs
+++ api/Program.cs
@@ -1,9 +1,21 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connectionString = $"Server=localhost,1434;Database=FinShark;User Id=SA;Password={Environment.GetEnvironmentVariable("SA_PASSWORD")};Encrypt=False;";
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
