693cba1a5b69b6452fbb3c9688b89a87f4656246
caowens <caowens555@yahoo.com>
2025-11-24 00:21:15 -0800

    Add filtering

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
@@ -24,14 +25,14 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetStocks()
+        public async Task<IActionResult> GetStocks([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
 
-            var stocks = await _stockRepo.GetAllAsync();
+            var stocks = await _stockRepo.GetAllAsync(query);
             var stockDto = stocks.Select(s => s.ToStockDto());
             
             return Ok(stockDto);
@@ -95,7 +96,7 @@ namespace api.Controllers
             {
                 return BadRequest(ModelState);
             }
-            
+
             var stockModel = await _stockRepo.DeleteAsync(id);
 
             if (stockModel == null)
