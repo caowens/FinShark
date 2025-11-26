0b49b290b9e95c573428e4d21c81df1b026a0417
caowens <caowens555@yahoo.com>
2025-11-26 15:28:04 -0800

    Enable CORS for frontend integration

--- api/Program.cs
+++ api/Program.cs
@@ -110,6 +110,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials()
+    .SetIsOriginAllowed(origin => true));
+
 app.UseAuthentication();
 app.UseAuthorization();
 
